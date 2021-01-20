2606d519886e0d64b17433b11cd2d11b3dda7dea
Rudraveer2805 <75520856+Rudraveer2805@users.noreply.github.com>
2021-01-20 13:01:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,69 @@
+var starImg, fairyImg, bgImg;
+var fairy , fairyVoice;
+var star, starBody;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	starImg = loadImage("images/star.png");
+	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
+	bgImg = loadImage("images/starNight.png");
+	//fairyVoice = loadSound("sound/JoyMusic.mp3");
+
+}
+
+function setup() {
+	createCanvas(800, 750);
+
+	// fairyVoice.play();
+
+	fairy = createSprite(130, 520);
+	fairy.addAnimation("fairyflying",fairyImg);  
+	fairy.scale =0.25;
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+
+}
+
+
+function draw() {
+  background(bgImg);
+
+
+
+ keyPressed("right","left","down");
+ 
+console.log(star.y);
+console.log(fairy.x);
+
+  drawSprites();
+
+}
+
+function keyPressed(Key1,Key2,Key3) {
+	//write code here
+	if (keyDown(Key1)) {
+		fairy.x = fairy.x+5;		
+	}
+	if (keyDown(Key2)) {
+		fairy.x = fairy.x-5;		
+	}
+    if (keyDown(Key3)) {
+		star.velocityY=2;
+	}
+
+}
